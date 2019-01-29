c5948c7659ccb184100dcf10122224302c73e4ac
Jake Thompson <b688nf@yahoo.co.uk>
2019-01-29 13:26:21 +0000

    Lambda function complete to read body for dynamic values to be injected into logic to fetch files using FTP

--- Lambda/Lambda SQS Part 2 FINAL.py
+++ Lambda/Lambda SQS Part 2 FINAL.py
@@ -3,19 +3,35 @@ import boto3
 from ftplib import FTP
 
 def lambda_handler(event, context):
+    #---------------------------------------------------------------------------------------------------------
+    # Reading payload and prep
+    #---------------------------------------------------------------------------------------------------------
+    body = json.loads(json.dumps(event)) #convert JSON object to python object
+    #---------------------------------------------------------------------------------------------------------
+    # Initiating file transfer
+    #---------------------------------------------------------------------------------------------------------
     ftp = FTP('3.8.173.47') #New FTP instance with connection setting for EC2 instance
     ftp.login('AWS',"") #establish connection to server with login creds
     files = ftp.dir() #variable of local directory
     print (files) #print local directory
-    ftp.cwd('/External/') #navigate to directory on server where file is located
+    ftp.cwd(body['location']) #navigate to directory on server where file is located
     print (ftp.pwd())
     print (ftp.dir())
-    responseBody = open('/tmp/sample.json','wb') #Create local file on temp lambda storage
-    ftp.retrbinary('RETR sample.json', responseBody.write, 1024) #retrieve file on server
+    responseBody = open('/tmp/' + body['filename'],'wb') #Create local file on temp lambda storage
+    ftp.retrbinary('RETR ' + body['filename'], responseBody.write, 1024) #retrieve file on server
     ftp.quit() #close connection
     responseBody.close() #close file IO stream
-    text = open("/tmp/sample.json") #Open file for logging
+    text = open("/tmp/" + body['filename']) #Open file for logging
     print ("Logging out file contents from temp lambda storage \n" + text.read()) #read out file contents
+    #---------------------------------------------------------------------------------------------------------
+    # Pushing file to s3 bucket
+    #---------------------------------------------------------------------------------------------------------
+
+    s3 = boto3.resource('s3')
+    s3bucket = "file-transfer-storage-poc"
+
+    s3.Bucket(s3bucket).upload_file("/tmp/" + body['filename'], body['filename'])
+    print ("S3 Accepted file with no error")
     return {
         'statusCode': 200,
         'body': "File successfully retreived and put into S3 Bucket" #Return response
