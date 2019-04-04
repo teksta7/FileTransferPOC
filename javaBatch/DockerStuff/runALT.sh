2743fd978aece34d8237f170c36233ba4a408361
Jake Thompson <b688nf@yahoo.co.uk>
2019-04-04 11:03:41 +0100

    Updated Dockerfile to connect WildFly with MYSQL database, run script also ammended to facilitate this

--- javaBatch/DockerStuff/runALT.sh
+++ javaBatch/DockerStuff/runALT.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 nohup sh /opt/jboss/wildfly/bin/standalone.sh -b 0.0.0.0 -bmanagement=0.0.0.0 &>/tmp/storage/WildFly.log &
 echo "Started Wildfly"
+sleep 8s
+sh /opt/jboss/wildfly/bin/jboss-cli.sh --connect --file=/tmp/storage/commands.cli
 cd /tmp/JBeretUI
 #nohup npm start &>/tmp/storage/JBeretUI.log &
 nohup gulp --restUrl http://localhost:8080/java-batch/api/ &> /tmp/storage/JBeretUI.log &
